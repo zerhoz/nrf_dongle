663089bd88f8c5c712ee34b15798faf27275a439
Vincent <thanhvinhvincent.nguyen@epfl.ch>
2023-02-15 10:56:29 -0800

    Added comments for aloidia bootloader configuration

---
+++ src/boards/aloidia/board.h
@@ -0,0 +1,67 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Yihui Xiong for Makerdiary
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _ALOIDIA_H_
+#define _ALOIDIA_H_
+
+#define _PINNUM(port, pin)    ((port)*32 + (pin))
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER           2
+#define LED_PRIMARY_PIN       _PINNUM(1, 7) // Blue (P1.07)
+#define LED_SECONDARY_PIN     _PINNUM(1, 5) // Green (P1.05)
+#define LED_STATE_ON          1
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTONS_NUMBER        2
+#define BUTTON_1              _PINNUM(0, 18) // unusable: RESET (P0.18)
+#define BUTTON_2              _PINNUM(0, 12) // no connection
+#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+#define BLEDIS_MANUFACTURER   "Vincent Nguyen"
+#define BLEDIS_MODEL          "aloidia Mechanical Keyboard"
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+#define USB_DESC_VID            0x239A
+#define USB_DESC_UF2_PID        0xA01A
+#define USB_DESC_CDC_ONLY_PID   0xA01A
+
+//--------------------------------------------------------------------+
+// UF2
+//--------------------------------------------------------------------+
+#define UF2_PRODUCT_NAME        "aloidia Mechanical Keyboard"
+#define UF2_VOLUME_LABEL        "ALOIDIA"
+#define UF2_BOARD_ID            "ALOIDIA"
+#define UF2_INDEX_URL           "https://github.com/nguyen-v/aloidia"
+
+
+#endif /* _ALOIDIA_H_ */
